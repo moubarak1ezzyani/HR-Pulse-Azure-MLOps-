f24348dccdbfdd523e5d1d1da7aa7238a0ff4885
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-24 18:38:43 +0000

    feat: setup config.py with global absolute paths

--- src/config.py
+++ src/config.py
@@ -1,1 +1,8 @@
 # Variables globales, chemins
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = os.path.join(BASE_DIR, "data", "processed", "df_hr_cleaned.csv")
+MODEL_DIR = os.path.join(BASE_DIR, "models")
+MODEL_PATH = os.path.join(MODEL_DIR, "saved_models", "salary_predictor.pkl") # régression
