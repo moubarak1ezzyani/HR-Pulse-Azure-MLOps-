1583bd0037e09aab493082aa395c55c413ea2a17
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-25 17:24:01 +0000

    feat(infra): add remote backend config and Azure AI Language service

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  # --- CONFIGURATION DU REMOTE BACKEND ---
+  # Demande ces infos à ton formateur si tu ne les as pas
+  backend "azurerm" {
+    resource_group_name  = "RG-HR-PULSE-MGMT-YENNAYA"
+    storage_account_name = "<NOM_DU_STORAGE_ACCOUNT_CENTRAL>"
+    container_name       = "tfstate"
+    key                  = "moubarak.terraform.tfstate" # Ton prénom pour ne pas écraser les autres
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-# 1. On pointe vers le serveur du formateur
+# 1. On pointe vers le serveur SQL du formateur
 data "azurerm_mssql_server" "formateur_server" {
   name                = "sql-server-hr-pulse-2026"
-  resource_group_name = "RG-HR-PULSE-MGMT-YENNAYA" # Ton RG de formateur
+  resource_group_name = "RG-HR-PULSE-MGMT-YENNAYA"
 }
+
+# 2. Ta base de données (Déjà créée, Terraform ne fera rien de plus)
 resource "azurerm_mssql_database" "db_student" {
   name      = "db-moubarak"
   server_id = data.azurerm_mssql_server.formateur_server.id
@@ -16,6 +36,24 @@ resource "azurerm_mssql_database" "db_student" {
   min_capacity = 0.5
   max_size_gb  = 2
 
-  # L'option magique pour économiser :
   auto_pause_delay_in_minutes = 15
+}
+
+# 3. NOUVEAU : Création du service Azure AI Language (NER)
+resource "azurerm_cognitive_account" "ai_language_student" {
+  name                = "cog-hr-pulse-moubarak"
+  location            = "westeurope" # Doit correspondre à la région du RG
+  resource_group_name = "RG-HR-PULSE-MGMT-YENNAYA"
+  kind                = "TextAnalytics"
+  sku_name            = "F0" # F0 est le Free Tier (Gratuit)
+}
+
+# 4. On expose les variables nécessaires pour le script Python (.env)
+output "ai_endpoint" {
+  value = azurerm_cognitive_account.ai_language_student.endpoint
+}
+
+output "ai_key" {
+  value     = azurerm_cognitive_account.ai_language_student.primary_access_key
+  sensitive = true
 }
