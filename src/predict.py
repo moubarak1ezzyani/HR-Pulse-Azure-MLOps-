557799400a833e8da004983171df90de7d85b083
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-26 13:25:41 +0000

    file cleaning

--- src/predict.py
+++ src/predict.py
@@ -4,21 +4,19 @@ import os
 from config import MODEL_PATH
 
 def predict_salary(description, size_avg, revenue_avg):
-    # 1. Charger le modèle (le pipeline complet : préprocesseur + RF)
+    # load model (préprocesseur + RF)
     if not os.path.exists(MODEL_PATH):
         return "Erreur : Le modèle n'a pas été trouvé. Lancez d'abord train.py."
     
     model = joblib.load(MODEL_PATH)
     
-    # 2. Préparer les données d'entrée dans le même format que l'entraînement
-    # Le modèle attend les colonnes : 'cleaned_description', 'Size_Avg', 'Revenue_Avg_Millions'
     input_data = pd.DataFrame([{
         'cleaned_description': description,
         'Size_Avg': size_avg,
         'Revenue_Avg_Millions': revenue_avg
     }])
     
-    # 3. Faire la prédiction
+    
     prediction = model.predict(input_data)
     
     return prediction[0]
@@ -28,8 +26,8 @@ if __name__ == "__main__":
     print("🔮 Test de prédiction en cours...")
     
     sample_desc = "Looking for a Data Scientist with Python, SQL and Machine Learning experience."
-    sample_size = 500  # Taille moyenne de l'entreprise
-    sample_rev = 100   # Revenu moyen en millions
+    sample_size = 500  # avg size 
+    sample_rev = 100   # avg revenue : milions
     
     salary = predict_salary(sample_desc, sample_size, sample_rev)
     
